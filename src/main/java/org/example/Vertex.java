064ff9aa31ba284e641ba727bf35f7b1c080de0b
erkenaz <erkenazazat08@gmail.com>
2026-05-09 19:18:57 +0500

    feat(vertex): implemented Vertex class

---
+++ src/main/java/org/example/Vertex.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Vertex {
+    private int id;
+
+    public Vertex(int id) {
+        this.id=id;
+    }
+    public int getId() {
+        return id;
+    }
+    @Override
+    public String toString() {
+        return "V"+id;
+    }
+}
